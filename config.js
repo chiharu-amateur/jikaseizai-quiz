7fc198c03555c7897e6e89c35d3254b82eb1772b
chiharu-amateur <chiharu.1516@gmail.com>
2026-07-01 13:13:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script のウェブアプリURLをここに貼り付けてください。
 // 例: const GAS_WEB_APP_URL = "https://script.google.com/macros/s/xxxx/exec";
-const GAS_WEB_APP_URL = "";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw0Bm2PK_xUGjLsNIvVciUYS4B6cDui4h07dJwGLoSUfjdCdy-MWVbyhpoxTrM3EhNU5w/exec";
